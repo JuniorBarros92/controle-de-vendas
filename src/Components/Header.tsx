5e18954f4912916c46f9667d220e482d5ed52dd8
Junior Barros <junior.dnbj55@gmail.com>
2026-03-22 20:03:36 -0300

    Slide Nav

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,11 +4,13 @@ import DateRanger from "./DateRanger";
 import Meses from "./Meses";
 
 const Header: React.FC = () => {
+  const [title, setTitle] = React.useState("Resumo");
 
   return (
-   <header className="mb">
-      <div className="mb">
+    <header className="mb">
+      <div className="daterange mb">
         <DateRanger />
+        <h1 className="box bg-3">{title}</h1>
       </div>
       <Meses />
     </header>
