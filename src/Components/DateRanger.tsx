a1bce802533e086a90394bd7c40a0d1d56b83a15
Junior Barros <junior.dnbj55@gmail.com>
2026-03-22 16:51:20 -0300

    Data Reativa

--- src/Components/DateRanger.tsx
+++ src/Components/DateRanger.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import DateInput from "./DateInput";
+import { useData } from "../Context/DataContext";
 
 const DateRanger: React.FC = () => {
- const [inicio, setInicio] = React.useState("");
- const [final, setFinal] = React.useState("");
-
+  const { inicio, setInicio, final, setFinal } = useData();
 
   return (
-    <form className="box flex" onSubmit={(e) => e.preventDefault() }>
-      <DateInput label="Início" value={inicio} onChange={({ target}) => setInicio(target.value)} />{inicio}
-      <DateInput label="Final" value={final} onChange={({ target }) => setFinal(target.value)} />{final}
+    <form className="box flex" onSubmit={(e) => e.preventDefault()}>
+      <DateInput
+        label="Início"
+        value={inicio}
+        onChange={({ target }) => setInicio(target.value)}
+      />
+      {inicio}
+      <DateInput
+        label="Final"
+        value={final}
+        onChange={({ target }) => setFinal(target.value)}
+      />
+      {final}
     </form>
   );
 };
--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -2,37 +2,53 @@ import React from "react";
 import useFetch from "../Hooks/useFetch";
 
 type IdataContext = {
-    data: Ivenda[] | null;
-    loading: boolean;
-    error: string | null;
-}
+  data: Ivenda[] | null;
+  loading: boolean;
+  error: string | null;
+  inicio: string;
+  final: string;
+  setInicio: React.Dispatch<React.SetStateAction<string>>;
+  setFinal: React.Dispatch<React.SetStateAction<string>>;
+};
 
 type Ivenda = {
-    id: string;
-    nome: string;
-    preco: number;
-    status: "pago" | "processando" | "falha"
-    pagamento: "boleto" | "pix" | "cartao";
-    data: string;
-    parcelas: number | null;
-}
+  id: string;
+  nome: string;
+  preco: number;
+  status: "pago" | "processando" | "falha";
+  pagamento: "boleto" | "pix" | "cartao";
+  data: string;
+  parcelas: number | null;
+};
 
-const Datacontext = React.createContext<IdataContext | null> (null)
+const Datacontext = React.createContext<IdataContext | null>(null);
 
 export const useData = () => {
-    const context = React.useContext(Datacontext);
-    if (!context) throw new Error ("useData precisa estar em DataContextProvider");
-        return context;
+  const context = React.useContext(Datacontext);
+  if (!context) throw new Error("useData precisa estar em DataContextProvider");
+  return context;
+};
 
+function getDate(n: number) {
+  const date = new Date();
+  date.setDate(date.getDate() - n);
+  const dd = String(date.getDate()).padStart(2, "0");
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
+  const yyyy = date.getFullYear();
+  return `${yyyy}-${mm}-${dd}`;
 }
 
-export const DataContextProvider = ({children} : React.PropsWithChildren) => {
-    const {data, loading, error} = useFetch<Ivenda[]>(
-        "https://data.origamid.dev/vendas/",
-    );
-    return (
-        <Datacontext.Provider value={{data, loading, error}}>
-            {children}
-        </Datacontext.Provider>
-    )
-}
+export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
+  const [inicio, setInicio] = React.useState(getDate(30));
+  const [final, setFinal] = React.useState(getDate(0));
+  const { data, loading, error } = useFetch<Ivenda[]>(
+    `https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`,
+  );
+  return (
+    <Datacontext.Provider
+      value={{ data, loading, error, inicio, setInicio, final, setFinal }}
+    >
+      {children}
+    </Datacontext.Provider>
+  );
+};
