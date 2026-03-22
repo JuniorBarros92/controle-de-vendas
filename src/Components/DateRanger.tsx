4ac180873715ae5aaeb9ca56fd02ec81b5c228c6
Junior Barros <junior.dnbj55@gmail.com>
2026-03-22 16:03:48 -0300

    CSS Base

--- src/Components/DateRanger.tsx
+++ src/Components/DateRanger.tsx
@@ -7,7 +7,7 @@ const DateRanger: React.FC = () => {
 
 
   return (
-    <form onSubmit={(e) => e.preventDefault() }>
+    <form className="box flex" onSubmit={(e) => e.preventDefault() }>
       <DateInput label="Início" value={inicio} onChange={({ target}) => setInicio(target.value)} />{inicio}
       <DateInput label="Final" value={final} onChange={({ target }) => setFinal(target.value)} />{final}
     </form>
