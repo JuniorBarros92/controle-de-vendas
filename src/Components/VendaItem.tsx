ccf9922c9112bba799592ce8cfb9247af5ded542
Junior Barros <junior.dnbj55@gmail.com>
2026-03-22 18:07:52 -0300

    Venda Item

---
+++ src/Components/VendaItem.tsx
@@ -0,0 +1,20 @@
+import type { Ivenda } from "../Context/DataContext";
+
+const VendaItem = ({ venda }: { venda: Ivenda }) => {
+  return (
+    <div className="venda box">
+      <a href="" style={{ fontFamily: "monospace" }}>
+        {venda.id}
+      </a>
+      <div>{venda.nome}</div>
+      <div>
+        {venda.preco.toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default VendaItem;
